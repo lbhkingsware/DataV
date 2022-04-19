d8d2eac170afebfff966a64977af01d381676267
liangbh <liangbh@kingsware.cn>
2022-04-19 14:07:32 +0800

    处理背景图问题

--- lib/components/scrollBoard/src/main.vue
+++ lib/components/scrollBoard/src/main.vue
@@ -33,7 +33,7 @@
         :style="`
           height: ${heights[ri]}px;
           line-height: ${heights[ri]}px;
-          background: ${rows.background};
+          background: ${row.background};
           background-color: ${
             mergedConfig[row.rowIndex % 2 === 0 ? 'evenRowBGC' : 'oddRowBGC']
           };
